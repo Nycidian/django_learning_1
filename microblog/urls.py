fc000054ddd57c78292f06b02c7d0e5f4954fa95
Grey Nycian <greynycian@gmail.com>
2014-03-13 06:04:32 +0000

    Update added index template

--- microblog/urls.py
+++ microblog/urls.py
@@ -10,6 +10,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'microblog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r"^$", TemplateView.as_view(template_name="index.html")),
     url(r'^admin/', include(admin.site.urls)),
 )
