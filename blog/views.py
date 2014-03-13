756844b1b4e01b02046d308acbf212be0a546dea
Grey Nycian <greynycian@gmail.com>
2014-03-13 09:17:11 +0000

    blog creation tutotorial 35 min

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
+from .models import Post
 # Create your views here.
+
+class PublishPostMixin(object):
+    def get_queryset(self):
+        queryset = super(PublishPostMixin, self).get_queryset()
+        return queryset.filter(published=True)
+
+class PostListView(PublishPostMixin, ListView):
+    model = Post
+
+    
+
+class PostDetailView(PublishPostMixin, DetailView):
+    model = Post
--- microblog/urls.py
+++ microblog/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+#from django.views.generic import TemplateView
 
+from . import views
 
 
 admin.autodiscover()
@@ -10,6 +11,12 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'microblog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r"^$", TemplateView.as_view(template_name="index.html")),
+    #url(r"^$", TemplateView.as_view(template_name="index.html")),
+
+    #default
+    url(r"^$", views.HomepageView.as_view(), name="home"),
+
+    url(r"^blog/", include("blog.urls", namespace="blog")),
+
     url(r'^admin/', include(admin.site.urls)),
 )
