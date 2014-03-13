2b1fe7de9f432d4ee580cc625de928e20a93a381
Grey Nycian <greynycian@gmail.com>
2014-03-13 05:39:17 +0000

    Update forgot to git add . a few times

---
+++ microblog/settings/base.py
@@ -0,0 +1,130 @@
+"""
+Django settings for microblog project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+
+import os
+
+import dj_database_url
+
+
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# here() gives us file paths from the root of the system to the directory
+# holding the current file.
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+PROJECT_ROOT = here("..")
+# root() gives us file paths from the root of the system to whatever
+# folder(s) we pass it starting at the parent directory of the current file.
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '(2ix&ht@w$65=oy5aspv=cv@1^0d@y+66@7bz5hj&rd&y(v#^2'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Grey Nycian', 'greynycidian@example.com'), 
+)
+
+MANAGERS = ADMINS
+
+
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'aqueous-brushlands-3135.herokuapp.com/']
+
+
+# Application definition
+
+DJANGO_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+THIRD_PARTY_APPS = (
+    
+)
+
+LOCAL_APPS = (
+    
+)
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+    
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'microblog.urls'
+
+WSGI_APPLICATION = 'microblog.wsgi.application'
+
+
+# Database Default
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(),
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+
+
+#directories
+
+MEDIA_ROOT = root("..","uploads")
+MEDIA_URL = ""
+
+
+STATIC_ROOT = root("..","static")
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    root("assets"),
+)
+
+TEMPLATE_DIRS = (
+    root("templates"),
+)
