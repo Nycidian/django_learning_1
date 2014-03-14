775e0122d341bd087231bbaed4b29965ce14675a
Grey Nycian <greynycian@gmail.com>
2014-03-14 10:52:20 +0000

    Fixed ALLOWED_HOSTS

--- microblog/settings/base.py
+++ microblog/settings/base.py
@@ -42,7 +42,7 @@ MANAGERS = ADMINS
 
 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
 
 # Application definition
